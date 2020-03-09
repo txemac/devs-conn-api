172dbade75e8fe148a52e155473a65158eaf096b
txemac <txema.bermudez@gmail.com>
2020-03-09 14:10:17 +0100

    Improve init database.

--- src_ddd/infrastructure/persistence/alchemy/model/__init__.py
+++ src_ddd/infrastructure/persistence/alchemy/model/__init__.py
@@ -1,9 +1,10 @@
-from config.persistence import database_settings
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine(database_settings.SQLALCHEMY_DATABASE_URI)
+engine = create_engine(os.getenv('DATABASE_URL'))
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
@@ -16,6 +17,10 @@ def add(obj):
     session.add(obj)
     try:
         session.commit()
-    except:
+    except Exception:
         session.rollback()
         raise
+
+
+from infrastructure.persistence.alchemy.model.dev import Dev
+from infrastructure.persistence.alchemy.model.register import Register
